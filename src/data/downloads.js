27356de0637176d8d39d0e89a8496cb4adcfa81a
YuWan_SAMA <145727116+YuWan886@users.noreply.github.com>
2025-10-25 11:22:44 +0800

    1.55:更新下载链接

--- src/data/downloads.js
+++ src/data/downloads.js
@@ -3,9 +3,31 @@ export const downloadData = {
   gskard: {
     name: "GSkard",
     description: "回合制卡牌PVP+PVE地图",
-    latestVersion: "1.52",
-    latestDate: "2025-09-21",
+    latestVersion: "1.55",
+    latestDate: "2025-10-18",
     downloads: [
+      {
+        version: "1.55",
+        date: "2025-10-18",
+        files: [
+          {
+            name: "GSkard V1.55.zip",
+            url: "https://wwje.lanzouu.com/iinO736onjwd",
+            info: "地图和资源包，先解压"
+          },
+          {
+            name: "GSkard-Modpack.mrpack",
+            url: "https://modrinth.com/modpack/gskard-modpack",
+            info: "整合包，自带资源包和地图，PCL&HMCL中搜 gskard 即可下载"
+          }
+        ],
+        changelog: [
+          "1.修复了神之一手选到没有牌的玩家会崩端的问题",
+          "2.凋零太刀在对局中传送到场地外现在会返回到友方场地",
+          "3.新增了几个进度",
+          "4.修复了无人机群的无人机首回合不攻击的问题"
+        ]
+      },
       {
         version: "1.52",
         date: "2025-09-21",
@@ -18,7 +40,7 @@ export const downloadData = {
           {
             name: "GSkard-Modpack.mrpack",
             url: "https://cdn.modrinth.com/data/KQK0AcoC/versions/q63ftYBF/%5B1.52%5DGSkard-Modpack.mrpack",
-            info: "整合包，自带资源包，无地图，PCL&HMCL中搜 gskard 即可下载"            
+            info: "整合包，自带资源包，无地图，PCL&HMCL中搜 gskard 即可下载"
           }
         ],
         changelog: [
