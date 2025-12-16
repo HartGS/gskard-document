00f834e6019e4ef845d6f50af7971c95b03d637f
YuWan_SAMA <145727116+YuWan886@users.noreply.github.com>
2025-12-16 19:44:55 +0800

    更新下载链接：1.6

--- src/data/downloads.js
+++ src/data/downloads.js
@@ -3,9 +3,39 @@ export const downloadData = {
   gskard: {
     name: "GSkard",
     description: "回合制卡牌PVP+PVE地图",
-    latestVersion: "1.55",
-    latestDate: "2025-10-18",
+    latestVersion: "1.6",
+    latestDate: "2025-12-14",
     downloads: [
+      {
+        version: "1.6",
+        date: "2025-12-14",
+        files: [
+          {
+            name: "GSkard v1.6.zip",
+            url: "https://wwavq.lanzouu.com/iHced3dml9ed",
+            info: "地图和资源包，先解压"
+          },
+          {
+            name: "GSkard-Modpack.mrpack",
+            url: "https://modrinth.com/modpack/gskard-modpack",
+            info: "整合包，自带资源包和地图，PCL&HMCL中搜 gskard 即可下载"
+          }
+        ],
+        changelog: [
+            "升啦！升啦！我终于把这个1.6端上来了家人们！",
+            "首先地图版本更新到了1.21.11",
+            "现在大厅的设置菜单全部一体化 统一用对话框来设置了",
+            "现在怪物的攻击力 护甲值 护甲韧性 移速都会随着回合数的增加有所提升",
+            "以后无限回合后期不再纯是简单局了",
+            "斧子现在可以横扫",
+            "新增了长矛的3种武器",
+            "新增了几只长矛相关的怪物",
+            "重做了飞升护符 天知道这玩意我写了多久",
+            "调整了部分天赋 新增了好几个天赋",
+            "现在凋零太刀不会瞬移出墙了 应该？不过就算出去了也没事 凋零太刀的突进后续会同步成长矛的突进",
+            "新增了几张法术牌"
+        ]
+      },
       {
         version: "1.55",
         date: "2025-10-18",
